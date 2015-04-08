116a3b41ecbe4017d8c582c4dd9f904dc38a46c3
Gerson Scanapieco <gerson.scanapieco@gmail.com>
2015-04-08 17:16:28 -0300

    Add funcionalidade de editar :nome e :email do Usuario

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,26 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @user = current_user
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to edit_user_path(@user), notice: "Alterações salvas com sucesso"
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  login_user
 
+  describe "#update" do
+    context "successfull update" do
+      before(:each) do
+        put :update, { id: @user.id, user: { name: "new_name" } }
+      end
+
+      it "updates the current user with the given information" do        
+        @user.reload  
+        expect(@user.name).to eq("new_name")
+      end
+
+      it "redirects to :edit" do
+        expect(response).to redirect_to edit_user_path(@user)
+      end
+
+      it "sets a success message" do
+        expect(flash.notice).to eq "Alterações salvas com sucesso" 
+      end
+    end
+
+    context "unsuccessfull update" do
+      before(:each) do
+        put :update, { id: @user.id, user: { email: "" } }
+      end
+
+      it "renders :edit view" do
+        expect(response).to render_template :edit
+      end
+    end
+  end
 end
